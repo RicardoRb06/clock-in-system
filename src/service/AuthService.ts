20555aa8ead517b98f3dab795e18da68f25df423
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-07-29 03:09:47 -0300

    feat: add return

--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import { User } from "../model/User.js";
 import { UserRepository } from "../repository/UserRepository.js";
 import type { RegisterRequestDto } from "../dto/auth/request/RegisterRequest.js";
+import type { RegisterResponseDto } from "../dto/auth/response/RegisterResponse.js";
 
 export class AuthService {
 
@@ -11,9 +12,10 @@ export class AuthService {
         this.userRepository = userRepository;
     }
 
-    public async register(request: RegisterRequestDto) {
+    public async register(request: RegisterRequestDto): Promise<RegisterResponseDto> {
         const hash = await bcrypt.hash(request.password, 10);
         const user = new User(request.name, hash);
         this.userRepository.save(user);
+        return { name: user.name };
     }
 }
