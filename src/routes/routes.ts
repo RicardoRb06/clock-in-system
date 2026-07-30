6576c9d657227e509551aa45091a641b3459d844
pedrohz1 <178624047+pedrohz1@users.noreply.github.com>
2026-07-30 03:13:56 -0300

    feat: create routes

---
+++ src/routes/routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { UserRepository } from '../repository/UserRepository.js';
+import { AuthService } from '../service/AuthService.js';
+import { AuthController } from '../controller/AuthController.js';
+import { AuthMiddleware } from '../middlewares/AuthMiddleware.js';
+
+export const routes: Router = Router();
+
+const userRepository = new UserRepository();
+
+const authService = new AuthService(userRepository);
+
+const authController = new AuthController(authService);
+
+const jwtSecret = process.env.JWT_SECRET;
+if (!jwtSecret) {
+    throw new Error("JWT_SECRET_MISSING: Defina a chave secreta no arquivo .env antes de rodar o app.");
+}
+
+const authMiddleware = new AuthMiddleware(jwtSecret);
+
+routes.post('/auth/register', authController.register);
+
+routes.post('/auth/login', authController.login);
