f718ce4ab825bc75e998c4aae4dcbc1d9e4b011c
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-07-30 03:04:33 -0300

    feat: change prisma

--- tests/helpers/setupDB.ts
+++ tests/helpers/setupDB.ts
@@ -1,13 +1,31 @@
 import { PostgreSqlContainer, StartedPostgreSqlContainer } from "@testcontainers/postgresql";
-import { Pool } from "pg";
+import { execSync } from 'child_process';
+import { PrismaClient } from '@prisma/client';
+
+let container: StartedPostgreSqlContainer;
+let prisma: PrismaClient;
 
 export async function startDatabase() {
     const container = await new PostgreSqlContainer("postgres:15.3").start();
-    const pool = new Pool({ connectionString: container.getConnectionUri() });
-    return { container, pool };
+    
+    const databaseUrl = container.getConnectionUri();   
+
+    execSync(`pnpm dlx prisma migrate deploy`, {
+        env: {
+            ...process.env,
+            DATABASE_URL: databaseUrl,
+        },
+    });
+
+    prisma = new PrismaClient({
+        datasources: { db: { url: databaseUrl } },
+    });
+
+
+    return { prisma, container };
 }
 
-export async function stopDatabase(container: StartedPostgreSqlContainer, pool: Pool) {  
-    await pool.end();
-    await container.stop();
+export async function stopDatabase() {  
+    await prisma?.$disconnect();
+    await container?.stop();
 }
