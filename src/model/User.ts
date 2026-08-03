1330c8466484c5a1fb1d14c9a9b4a34b1d580780
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-03 00:21:31 -0300

    fix: remove readonly

--- src/model/User.ts
+++ src/model/User.ts
@@ -5,7 +5,7 @@ export enum ROLES {
 }
 
 export class User {
-    private readonly _id: string;
+    private _id: string;
     private _name: string;
     private _passwordHash: string;
     private _isWorking: boolean;
