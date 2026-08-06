8199d95456278f26a41d85d5f5c788b9ffa1104e
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-06 20:21:00 -0300

    fix: remove request dto and change to data

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,8 +1,6 @@
 import bcrypt from "bcryptjs";
 import { User } from "../model/User.js";
 import { UserRepository } from "../repository/user.repository.js";
-import type { RegisterRequestDto } from "../dto/auth/request/register.request.js";
-import type { LoginRequestDto } from "../dto/auth/request/login.request.js";
 import jwt from 'jsonwebtoken';
 import { env } from "../config/env.js";
 
@@ -20,17 +18,17 @@ export class AuthService {
         this.userRepository = userRepository;
     }
 
-    public async register(request: RegisterRequestDto): Promise<Output> {
+    public async register(data: { name: string; password: string }): Promise<Output> {
 
-        if(await this.userRepository.existsByName(request.name)) {
+        if(await this.userRepository.existsByName(data.name)) {
             return { 
                 success: false,
                 error: "Nome de usuário já existe"
             }
         }
 
-        const hash = await bcrypt.hash(request.password, 10);
-        const user = new User(request.name, hash);
+        const hash = await bcrypt.hash(data.password, 10);
+        const user = new User(data.name, hash);
         
         await this.userRepository.save(user);
 
@@ -46,9 +44,9 @@ export class AuthService {
          };
     }
 
-    public async login(request: LoginRequestDto): Promise<Output> {
+    public async login(data: { name: string; password: string }): Promise<Output> {
 
-        const user = await this.userRepository.findByName(request.name);
+        const user = await this.userRepository.findByName(data.name);
         
         if (!user) {
             return { 
@@ -57,7 +55,7 @@ export class AuthService {
             };
         }
 
-        const isMatch = await bcrypt.compare(request.password, user.passwordHash);
+        const isMatch = await bcrypt.compare(data.password, user.passwordHash);
         
         if (!isMatch) {
             return { 
