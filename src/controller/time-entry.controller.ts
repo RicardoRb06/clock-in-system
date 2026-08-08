fa74b0d1cd80b5bbcf412f6c5d7cbe5752f9e504
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-07 21:08:12 -0300

    fix: fix two time entry controller to just one

--- src/controller/time-entry.controller.ts
+++ src/controller/time-entry.controller.ts
@@ -1,24 +1,25 @@
-import type { Request, Response } from 'express';
+import { TimeEntryRequestSchema } from "../dto/time-entry/request/time-entry.request.js";
 import type { TimeEntryService } from "../service/time-entry.service.js";
-import { TimeEntryRequestSchema } from '../dto/time-entry/request/time-entry.request.js';
+import type { Request, Response } from 'express';
 import { z } from 'zod';
 
 export class TimeEntryController {
+    
     private timeEntryService: TimeEntryService;
-
+    
     constructor(timeEntryService: TimeEntryService) {
         this.timeEntryService = timeEntryService;
     }
 
     async clockIn(req: Request, res: Response) {
-        const dto = TimeEntryRequestSchema.safeParse(req.body); 
+        const dto = TimeEntryRequestSchema.safeParse(req.body);
 
-        if(!dto.success) {
-            return res.status(400).json({
-                success: false,
+        if (!dto.success) {
+            return res.status(400).json({ 
+                success: false, 
                 message: "Dados de envio inválidos",
                 errors: z.flattenError(dto.error).fieldErrors
-            })
+            });
         }
 
         const result = await this.timeEntryService.clockIn(dto.data);
@@ -30,11 +31,33 @@ export class TimeEntryController {
             });
         }
 
-        return {
+        return res.status(200).json({
             success: true,
-            message: "Ponto aberto!",
-            error: null
+        });
+    }
+
+    async clockOut(req: Request, res: Response) {
+        const dto = TimeEntryRequestSchema.safeParse(req.body);
+
+        if (!dto.success) {
+            return res.status(400).json({ 
+                success: false, 
+                message: "Dados de envio inválidos",
+                errors: z.flattenError(dto.error).fieldErrors
+            });
         }
-        
+
+        const result = await this.timeEntryService.clockOut(dto.data);
+
+        if (!result.success) {
+            return res.status(400).json({ 
+                success: false, 
+                message: result.error 
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+        });
     }
 }
