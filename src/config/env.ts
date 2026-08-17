32315f6e285dc6307db7661e3a776fb463d73597
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-17 00:03:18 -0300

    feat: add cors to envSchema

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,14 @@
-import { z } from "zod";
+import { boolean, z } from "zod";
 
 const envSchema = z.object({
     JWT_SECRET: z.string().min(1, "JWT_SECRET_MISSING: A chave secreta do JWT não foi carregada do arquivo .env."),
     DATABASE_URL: z.string().min(1, "DATABASE_URL_MISSING: A URL do banco de dados não foi carregada do arquivo .env."),
+    CORS_ALLOWED_ORIGINS: z.string().min(1, "CORS_ALLOWED_ORIGINS_MISSING: As origns do CORS não foram carregadas do arquivo .env")
+    .transform((val) => {
+        val.split(",")
+        .map((origin) => origin.trim())
+        .filter(boolean)
+    })
 });
 
 export const env = envSchema.parse(process.env);
