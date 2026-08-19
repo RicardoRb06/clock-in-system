f6b18289e8bd2854dae536ae644d9253cd16efc9
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-19 14:31:17 -0300

    feat: add PORT to .env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,7 @@
 import { boolean, z } from "zod";
 
 const envSchema = z.object({
+    PORT: z.string().min(1, "PORT_MISSING: A porta não foi carregada do arquivo .env."),
     JWT_SECRET: z.string().min(1, "JWT_SECRET_MISSING: A chave secreta do JWT não foi carregada do arquivo .env."),
     DATABASE_URL: z.string().min(1, "DATABASE_URL_MISSING: A URL do banco de dados não foi carregada do arquivo .env."),
     CORS_ALLOWED_ORIGINS: z.string().min(1, "CORS_ALLOWED_ORIGINS_MISSING: As origns do CORS não foram carregadas do arquivo .env")
