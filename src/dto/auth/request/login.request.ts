54a309fe360387e9ab2bfbdddc7b4263d8a2a272
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-20 17:23:53 -0300

    feat: add new validations to password

--- src/dto/auth/request/login.request.ts
+++ src/dto/auth/request/login.request.ts
@@ -4,7 +4,8 @@ export const LoginRequestSchema = z.object({
     name: z.string()
         .min(1, "Campo 'nome' não pode ficar vazio"),
     password: z.string()
-        .min(6, "Campo 'senha' não pode ficar vazio"),
+        .min(8, "A senha deve ter no mínimo 8 caracteres")
+        .max(64, "A senha deve ter no máximo 64 caracteres")
 });
 
 export type LoginRequestDto = z.infer<typeof LoginRequestSchema>;
