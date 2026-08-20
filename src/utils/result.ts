1d4aea066f72dc7cc78f134db70da40c7307bacc
pedrohz1 <178624047+pedrohz1@users.noreply.github.com>
2026-08-20 17:49:27 -0300

    fix: data is optional

--- src/utils/result.ts
+++ src/utils/result.ts
@@ -1,10 +1,15 @@
 export type Result<T, E = Error> = 
-    | {success: true; data: T}
+    | {success: true; data?: T}
     | {success: false; error: E};
 
 
-export function ok<T>(data: T): Result<T> {
-    return { success: true, data: data};
+export function ok<T>(data?: T): Result<T> {
+    if(data) {
+        return { success: true, data: data};
+    }
+
+    return { success: true };
+    
 }
 
 export function err(error: Error): Result<never> {
