9ebcbbe96e2b368997b6da5bad5c125b80050320
pedrohz1 <178624047+pedrohz1@users.noreply.github.com>
2026-08-20 17:42:42 -0300

    refactor: prisma error map now is generic

--- src/utils/prisma.errors.ts
+++ src/utils/prisma.errors.ts
@@ -1,14 +1,19 @@
-import { Prisma } from '@prisma/client'
-import { DuplicateUserError } from '../errors/DuplicateUserError.js';
-import { User } from '../model/User.js';
+import { Prisma } from "@prisma/client";
+import { ConflictError } from "../errors/ConflictError.js";
+import { User } from "../model/User.js";
 
-export function isUniqueViolation(e: unknown): e is Prisma.PrismaClientKnownRequestError {
-    return e instanceof Prisma.PrismaClientKnownRequestError && e.code === 'P2002';
-}
+const ERROR_MAP: Record<string, new () => Error> = {
+  P2002: ConflictError,
+};
 
-export function mapUserError(e: unknown, user: User): Error {
-    if(isUniqueViolation(e)){
-        return new DuplicateUserError(user.name);
+export function mapError(e: unknown): Error {
+  if (e instanceof Prisma.PrismaClientKnownRequestError) {
+    const ErrorClass = ERROR_MAP[e.code];
+
+    if (ErrorClass) {
+      return new ErrorClass();
     }
-    return e as Error;
+  }
+
+  return e instanceof Error ? e : new Error(String(e));
 }
