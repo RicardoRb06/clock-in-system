873b0aa043cc066a0069b61f9fafe84dd2044a1c
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-20 17:29:36 -0300

    fix: change success bollean to success true

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -6,7 +6,7 @@ import { env } from "../config/env.js";
 import { DuplicateUserError } from "../errors/DuplicateUserError.js";
 
 type Output = 
-    | { success: boolean; role: string; token: string; }
+    | { success: true; role: string; token: string; }
     | { success: false; error: string; }
 
 
