f56a6b015bded3ef5dcc649348a5f6a6eeb51cf5
RicardoRb06 <ricardo.fernandes.ribeiro.06@gmail.com>
2026-08-26 16:44:32 -0300

    feat: create endpoint me

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from 'express';
 import { AuthService } from '../service/auth.service.js';
 import { AUTH_COOKIE_NAME, AUTH_COOKIE_OPTIONS } from '../config/cookies.js';
+import { success } from 'zod';
 
 export class AuthController {
     
@@ -44,5 +45,21 @@ export class AuthController {
             success: true,
             role: result.role,
         });
+    }
+
+    async me(req: Request, res: Response) {
+        const result = await this.authService.me(req.user.id);
+
+        if(!result.success){
+            return res.status(400).json({success: false,})
+        }
+
+        return res.status(200).json({
+            success: true,
+            name: result.name,
+            role: result.role,
+            category: result.category,
+        })
+
     }
 }
