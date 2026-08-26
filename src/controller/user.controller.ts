e74fa3cb7befcae5d34c4fe7507119c2dc814dc2
pedrohz1 <178624047+pedrohz1@users.noreply.github.com>
2026-08-25 23:30:37 -0300

    feat: add findById and findByName to user controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -8,6 +8,38 @@ export class UserController {
         this.userService = userService;
     }
 
+    public async findById(req: Request, res: Response) {
+        const result = await this.userService.findById(req.body.id);
+
+        if(!result.success) {
+            return res.status(400).json({
+                success: false,
+                message: result.error
+            })
+        }
+
+        return res.status(201).json({
+            success: true,
+            data: result.data,
+        });
+    }
+
+    public async findByName(req: Request, res: Response) {
+        const result = await this.userService.findByName(req.body.name);
+
+        if(!result.success) {
+            return res.status(400).json({
+                success: false,
+                message: result.error
+            })
+        }
+
+        return res.status(201).json({
+            success: true,
+            data: result.data,
+        });
+    }
+
     public async getUsers(req: Request, res: Response) {
         if(!req.body.page) {
             req.body.page = 1;
