818af0de944be3c16093669ecd7966a5b19be6dc
pedrohz1 <178624047+pedrohz1@users.noreply.github.com>
2026-09-01 22:49:02 -0300

    feat: controller response pattern now in result util

--- src/utils/result.ts
+++ src/utils/result.ts
@@ -1,3 +1,6 @@
+import type { Request, Response } from 'express';
+
+
 export type Result<T, E = Error> = 
     | {success: true; data?: T}
     | {success: false; error: E};
@@ -14,4 +17,18 @@ export function ok<T>(data?: T): Result<T> {
 
 export function err(error: Error): Result<never> {
     return { success: false, error: error};
+}
+
+export function complete<T>(res: Response, status: number, data: T) {
+    return res.status(status).json({
+        success: true,
+        data: data,
+    });
+}
+
+export function fail(res: Response, error: Error) {
+    return res.status(400).json({
+        success: false,
+        message: error.message
+    });
 }
