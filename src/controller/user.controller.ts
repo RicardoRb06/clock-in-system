faeed5c9ff26d9b0a226cc34b3c6381264461397
pedrohz1 <178624047+pedrohz1@users.noreply.github.com>
2026-09-01 22:50:51 -0300

    fix: complete and fail moved to utils

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express';
 import type { UserService } from "../service/user.service.js";
+import { complete, fail } from '../utils/result.js';
 
 export class UserController {
     private userService: UserService;
@@ -8,47 +9,33 @@ export class UserController {
         this.userService = userService;
     }
 
-    private complete<T>(res: Response, status: number, data: T) {
-        return res.status(status).json({
-            success: true,
-            data: data,
-        });
-    }
-
-    private fail(res: Response, error: Error) {
-        return res.status(400).json({
-            success: false,
-            message: error.message
-        });
-    }
-
     public async findById(req: Request, res: Response) {
         const result = await this.userService.findById(req.body.id);
 
         if(!result.success) {
-            return this.fail(res, result.error);
+            return fail(res, result.error);
         }
 
-        return this.complete(res, 201, result.data);
+        return complete(res, 201, result.data);
     }
 
     public async findByName(req: Request, res: Response) {
         const result = await this.userService.findByName(req.body.name);
 
         if(!result.success) {
-            return this.fail(res, result.error);
+            return fail(res, result.error);
         }
 
-        return this.complete(res, 201, result.data);
+        return complete(res, 201, result.data);
     }
 
     public async getUsers(req: Request, res: Response) {
         const result = await this.userService.getUsers();
 
         if(!result.success) {
-            return this.fail(res, result.error);
+            return fail(res, result.error);
         }
 
-        return this.complete(res, 201, result.data);
+        return complete(res, 201, result.data);
     }
 }
