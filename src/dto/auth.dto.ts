bd215fab4546a56a95a30dc546ca6cced4d56f81
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-11 18:58:43 -0300

    feat: update passaword requiriments at register

--- src/dto/auth.dto.ts
+++ src/dto/auth.dto.ts
@@ -4,7 +4,8 @@ export const RegisterRequestSchema = z.object({
     name: z.string()
         .min(1, "O nome é obrigatório"),
     password: z.string()
-        .min(6, "A senha deve ter pelo menos 6 caracteres"),
+        .min(8, "A senha deve ter no mínimo 8 caracteres")
+        .max(64, "A senha deve ter no máximo 64 caracteres")
 });
 
 export const LoginRequestSchema = z.object({
