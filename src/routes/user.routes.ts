7e9e2387f4df93bb2ae1f2f625cf1abe5906ce33
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-09-11 18:54:38 -0300

    feat: remove create new intances and export

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,18 +1,8 @@
 import { Router } from 'express';
-import { prisma } from '../database/prisma.js';
-import { env } from '../config/env.js';
-import { UserRepository } from '../repository/user.repository.js';
-import { UserService } from '../service/user.service.js';
-import { UserController } from '../controller/user.controller.js';
-import { AuthMiddleware } from '../middlewares/auth.middleware.js';
+import { userController } from '../controller/user.controller.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 export function createUserRoutes(): Router {
-    const userRepository = new UserRepository(prisma);
-    const userService = new UserService(userRepository);
-    const userController = new UserController(userService);
-
-    const authMiddleware = new AuthMiddleware(env.JWT_SECRET);
-
     const router: Router = Router();
 
     router.get('/get-all', authMiddleware.validate, (req, res) => userController.getUsers(req, res));
